78220b64443c2e72b5fa52ee02c4656f5b924479
Nikolai Cherkezishvili <nikolai.cherkezishvi@itaas.dimensiondata.com>
2016-07-28 13:32:37 +0100

    fixed a bug; added bootstraping ability

--- percona-5.1/entrypoint.sh
+++ percona-5.1/entrypoint.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-#/etc/init.d/mysql start
-mysqld_safe
+shopt -s nullglob
 
-exec "$@"
+function db_update 
+{
+  for SQL in /bootstrap/*.sql; do
+    echo "Applying sql file $SQL"
+    mysql -uroot -p'root' < "$SQL"
+  done
+}
+
+if [[ $# -eq 0 ]]; then
+  /etc/init.d/mysql start
+  db_update
+  /etc/init.d/mysql stop
+  mysqld_safe
+else 
+  /etc/init.d/mysql start
+  db_update
+  exec "$@"
+fi 
